0fdd12fd4e04078ff62ca7c4c0bf4489266c8ccf
Alexander Do <alexanderhdo@gmail.com>
2019-03-17 18:44:16 -0700

    finished the ssh connect class, still more left

--- test.py
+++ test.py
@@ -1,21 +1,34 @@
 import paramiko
 import sys
+import csv
 
 ssh = paramiko.SSHClient()
-# ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.load_system_host_keys(filename=None)
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# ssh.load_system_host_keys(filename=None)
 
 ssh.connect(hostname = '167.99.175.39',
-        username = 'root')
+        username = 'root',
+        password = 'test123')
         # gss_auth = True,
         # gss_kex = True)
 
 
 def get_free_memory(ssh_obj):
+    """
+    returns a dictionary of critical memory stats
+    """
     stdin, stdout, stderr = ssh_obj.exec_command('free -h')
     mem_stats = stdout.readlines()[1]
-    mem_stats_formatted = " ".join(mem_stats.split())
-    return mem_stats_formatted
+    _, total, used, free, shared, cache, avail = mem_stats.split()
+    memory_info = {
+            'total': total,
+            'used': used,
+            'free': free,
+            'shared': shared,
+            'cache': cache,
+            'available': avail
+    }
+    return memory_info
 
 
 def get_logged_in_users(ssh_obj):
@@ -32,10 +45,33 @@ def get_logged_in_users(ssh_obj):
 def get_disk_space(ssh_obj):
     stdin, stdout, stderr = ssh_obj.exec_command('df -h /')
     root_space = stdout.readlines()
-    return root_space[1]
+    disk, total_space, used_space, avail_space, _, _ = root_space[1].split()
+    disk_info = {
+            'disk_name' : disk,
+            'total_space' : total_space,
+            'used_space' : used_space,
+            'avail_space' : avail_space
+    }
+    return disk_info
+
+def save_to_csv(mydict):
+    # server_csv = open('server_test.csv', 'w')
+    # server_csv_writer = csv.DictWriter(server_csv,mydict.keys())
+    with open('server_test.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in mydict.items():
+            writer.writerow([key + ' mem', value])
+        # DICTIONARY COMPREHENSION
+    return None
+
+
+    
+
 
 
 print(get_free_memory(ssh))
+memory_stats = get_free_memory(ssh)
+save_to_csv(memory_stats)
 print(get_logged_in_users(ssh))
 print(get_disk_space(ssh))
 
@@ -44,7 +80,22 @@ ssh.close()
 
 '''
 LEFT TO DO: 
-1. SPIN UP DIGITAL OCEAN SERVER WITH SSH KEY FOR TESTING (CHECK)
-2. TURN THIS INTO A CLASS
-3. FIGURE OUT HOW TO RETURN MULTIPLE THINGS FROM THESE FUNCTIONS
+1. Write CSV functions
+2. 
+hostname,total mem,x,free mem,x,used mem,x
+logged in users,x
+disk space,x,usedspace, x
+
+1.  Use any data structure like list, dictionary, set or tuple (CHECK)
+2.  List comprehension 
+3.  Dictionary comprehension
+4.  Functions (CHECK)
+5.  Classes (CHECK)
+6.  User created iterators
+7.  Importing external modules (CHECK)
+8.  Error checks using try-except (NEED TO DO)
+9.  File input and output (CHECK)
+10. Regular expression
+11. Itertools 
+12. Decorators
 '''
--- test_class.py
+++ test_class.py
@@ -1,6 +1,93 @@
 import paramiko
+import sys
 
 class Server():
-    def __init__(self, hostname, user):
+    def __init__(self, hostname, user, password):
         self.hostname = hostname
         self.user = user
+        self.password = password
+        self.ssh_obj = self.connect()
+    
+    def connect(self):
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(
+            hostname = self.hostname,
+            username = self.user,
+            password = self.password
+        )
+        stdin, stdout, stderr = ssh.exec_command('ls')
+        return ssh
+    
+    def get_free_memory(self):
+        stdin, stdout, stderr = self.ssh_obj.exec_command('free -h')
+        mem_stats = stdout.readlines()[1]
+        _, total, used, free, shared, cache, avail = mem_stats.split()
+        memory_info = {
+                'total': total,
+                'used': used,
+                'free': free,
+                'shared': shared,
+                'cache': cache,
+                'available': avail
+        }
+        return memory_info
+
+    def get_logged_in_users(self):
+        """
+        Should do regex to return/yield the uptime, number of users, load average, and users
+        :param ssh_obj:
+        :return:
+        """
+        stdin, stdout, stderr = self.ssh_obj.exec_command('w')
+        logged_in = stdout.readlines()
+        num_users = len(logged_in) - 2
+        return {'logged in users': num_users}
+
+    def _disk_space(self):
+        stdin, stdout, stderr = self.ssh_obj.exec_command('df -h /')
+        root_space = stdout.readlines()
+        disk, total_space, used_space, avail_space, _, _ = root_space[1].split()
+        disk_info = {
+                'disk_name' : disk,
+                'total_space' : total_space,
+                'used_space' : used_space,
+                'avail_space' : avail_space
+        }
+        return disk_info
+
+    def get_stat(self, 'stat_code'):
+        if stat_code == 'disk_space':
+            return_dict = _disk_space
+        elif stats_code == 'logged_in_users':
+            return_dict = self.get_logged_in_users
+
+        for key, value in return_dict.items():
+            print(f'{key} is : {value}')
+
+    
+
+
+
+    def write_mem_to_csv(self):
+        pass
+
+s = Server('167.99.175.39', 'root', 'test123')
+t = s.get_free_memory()
+logged_in_users = s.get_logged_in_users()
+s.get_disk_space()
+s.write_mem_to_csv()
+print(s)
+print(t)
+print(logged_in_users)
+'''
+date/time,hostname,total mem,x,free mem,x,used mem,x
+logged in users,x
+disk space,x,usedspace,x
+
+1. Write function get_all_stats
+2. Finish write to csv function
+3. Include try-except in the connect method()
+4. Think of CLI flow
+'''
+
